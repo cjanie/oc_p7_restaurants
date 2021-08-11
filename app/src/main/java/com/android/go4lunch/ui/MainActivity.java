5358f1d754dc2d3f2609e8fcf9eda53428ba52aa
cjanie <janiechun@hotmail.fr>
2021-08-11 13:45:59 +0200

    location

--- app/src/main/java/com/android/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/android/go4lunch/ui/MainActivity.java
@@ -1,64 +1,53 @@
 package com.android.go4lunch.ui;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.Button;
 
 import com.android.go4lunch.R;
+import com.android.go4lunch.read.adapter.AndroidLocationProvider;
+import com.android.go4lunch.read.businesslogic.usecases.model.DistanceInfo;
+import com.android.go4lunch.read.businesslogic.usecases.model.Geolocation;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    private LocationViewModel locationViewModel;
+
+
+    @BindView(R.id.share_location_button)
+    public Button shareLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Location location = this.getHere();
+        ButterKnife.bind(this);
 
+        this.locationViewModel = new ViewModelProvider(this).get(LocationViewModel.class);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-        }
+    @OnClick(R.id.share_location_button)
+    public void onShareLocation() {
+        DistanceInfo distanceInfo = new DistanceInfo(new AndroidLocationProvider(this));
+        this.locationViewModel.setDistanceInfoValue(distanceInfo);
     }
-
-    private Location getHere() {
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            String context = Context.LOCATION_SERVICE;
-            LocationManager locationManager = (LocationManager) this.getSystemService(context);
-
-            String gpsProvider = LocationManager.GPS_PROVIDER;
-            Location location = locationManager.getLastKnownLocation(gpsProvider);
-            return location;
-        }
-        return null;
-
-    }
-
 }
