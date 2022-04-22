b6942493d1c8d05077cf146352c430822d1ebfc9
cjanie <janiechun@hotmail.fr>
2022-04-22 22:35:42 +0200

    logout

--- app/src/main/java/com/android/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/android/go4lunch/ui/MainActivity.java
@@ -3,13 +3,18 @@ package com.android.go4lunch.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.android.go4lunch.R;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,16 +51,23 @@ public class MainActivity extends BaseActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId() == R.id.settings) {
-
+                    Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                    startActivity(intent);
                 }
                 if(item.getItemId() == R.id.logout) {
-
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
                 drawerLayout.close();
-                return false;
+                return false; // corresponds to is checked
             }
         });
-
     }
 
+    @Override
+    public void onBackPressed() {
+        Snackbar.make(getWindow().getDecorView().getRootView(), R.string.on_back_pressed_disabled, Snackbar.LENGTH_LONG).show();
+    }
 }
