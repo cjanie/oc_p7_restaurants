0755dc86390bd60e9508abba24571f9202852be6
cjanie <janiechun@hotmail.fr>
2022-05-11 10:34:30 +0200

    web view fragment

---
+++ app/src/main/java/com/android/go4lunch/ui/fragments/DetailsFragment.java
@@ -0,0 +1,22 @@
+package com.android.go4lunch.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.go4lunch.R;
+
+public class DetailsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_details, container, false);
+        return root;
+    }
+}
