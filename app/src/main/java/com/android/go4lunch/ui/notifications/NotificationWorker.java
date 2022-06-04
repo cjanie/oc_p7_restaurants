6b1605aa2e6c4c9e903583645c4163435079e36a
cjanie <janiechun@hotmail.fr>
2022-06-04 03:55:35 +0200

    data provider to show notification action

--- app/src/main/java/com/android/go4lunch/ui/notifications/NotificationWorker.java
+++ app/src/main/java/com/android/go4lunch/ui/notifications/NotificationWorker.java
@@ -12,6 +12,7 @@ import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
@@ -27,38 +28,29 @@ import java.util.concurrent.TimeUnit;
 
 public class NotificationWorker extends Worker {
 
-    private NotificationManagerCompat notificationManager;
-
-    private NotificationWorkManagerProgram workManagerProgram;
-
-    private List<String> notificationTexts;
+    private WorkManager workManager;
 
     public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
-        this.notificationManager = NotificationManagerCompat.from(context);
-
-        this.workManagerProgram = new NotificationWorkManagerProgram(context);
-
-        this.notificationTexts = this.getNotificationTexts();
-
+        this.workManager= WorkManager.getInstance(context);
     }
 
     @NonNull
     @Override
     public Result doWork() {
+        new ShowNotificationAction(this.getApplicationContext()).run();
+
         Data data = this.createData(new NotificationValueObject(1, "Hy"));
 
         int notificationId = this.getInputData().getInt(NotificationConfig.NOTIFICATION_ID, 0);
         String notificationText = this.getInputData().getString(NotificationConfig.NOTIFICATION_TEXT);
         if(notificationText != null) {
             NotificationValueObject notificationVO = new NotificationValueObject(notificationId, notificationText);
-            NotificationCompat.Builder notificationBuilder = this.configureNotificationBuilder(notificationVO);
-            this.notificationManager.notify(notificationVO.getId(), notificationBuilder.build());
 
 
             data = this.createData(notificationVO);
             WorkRequest workRequest = this.createPeriodicWorkRequest(data);
-            this.workManagerProgram.enqueueWorkRequest(workRequest);
+            this.workManager.enqueue(workRequest);
 
         }
 
@@ -85,65 +77,4 @@ public class NotificationWorker extends Worker {
                 .setInputData(inputData)
                 .build();
     }
-
-    private NotificationCompat.Builder configureNotificationBuilder(NotificationValueObject notificationVO) {
-        // Configure Notification.Builder
-        return new NotificationCompat.Builder(this.getApplicationContext(), NotificationConfig.NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_baseline_person_24)
-                .setContentTitle("Go4Lunch Time")
-                .setContentText(notificationVO.getText())
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(this.createNotificationContentIntent(notificationVO))
-                .setAutoCancel(true);
-    }
-
-    private PendingIntent createNotificationContentIntent(NotificationValueObject notificationVO) {
-        // Create Intent and return PendingIntent
-        Intent notifyIntent = new Intent(this.getApplicationContext(), ActivityResult.class);
-        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        notifyIntent.putExtra(NotificationConfig.NOTIFICATION_ID, notificationVO.getId());
-        notifyIntent.putExtra(NotificationConfig.NOTIFICATION_TEXT, notificationVO.getText());
-
-        return PendingIntent.getActivity(
-                this.getApplicationContext(), 0, notifyIntent, PendingIntent.FLAG_IMMUTABLE
-        );
-    }
-
-    private List<NotificationValueObject> getNotificationValueObjects() {
-        List<NotificationValueObject> notificationVOs = new ArrayList<>();
-        if(!this.notificationTexts.isEmpty()) {
-            int notificationId = 0;
-            for(String notificationText: this.notificationTexts) {
-                notificationId += 1;
-                NotificationValueObject notificationVO = new NotificationValueObject(
-                    notificationId, notificationText
-                );
-                notificationVOs.add(notificationVO);
-            }
-        }
-        return notificationVOs;
-     }
-
-    private List<String> getNotificationTexts() {
-        List<String> notifications = new ArrayList<>();
-        if(!this.getSelections().isEmpty()) {
-            for(Selection selection: this.getSelections()) {
-                notifications.add(selection.getWorkmateName() + " is eating at " + selection.getRestaurantName());
-            }
-        }
-        return notifications;
-    }
-
-    private List<Selection> getSelections() {
-        // Mock
-        Selection selection1 = new Selection("resto1", "workmate1");
-        selection1.setWorkmateName("Janie");
-        selection1.setRestaurantName("Chez Jojo");
-        selection1.setId("selection1");
-
-        return Arrays.asList(selection1);
-    }
-
-
-
 }
--- app/src/main/java/com/android/go4lunch/ui/notifications/ShowNotificationAction.java
+++ app/src/main/java/com/android/go4lunch/ui/notifications/ShowNotificationAction.java
@@ -3,19 +3,27 @@ package com.android.go4lunch.ui.notifications;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.SystemClock;
 
+import androidx.activity.result.ActivityResult;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.android.go4lunch.R;
+import com.android.go4lunch.businesslogic.entities.Selection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
 
 public class ShowNotificationAction {
 
@@ -40,32 +48,62 @@ public class ShowNotificationAction {
     }
 
     public void run() {
-        List<String> notifs = Arrays.asList("notif-1", "notif-2");
-        if(!notifs.isEmpty()) {
-            for (int i = 0; i < notifs.size(); i++) {
-                SystemClock.sleep(2000);
-                this.notifyWorkmatesSelections(i, notifs.get(i));
+
+        if(!this.getNotificationTexts().isEmpty()) {
+            for (int i = 0; i < this.getNotificationTexts().size(); i++) {
+                this.notifyWorkmatesSelections(i, this.getNotificationTexts().get(i));
             }
         }
     }
 
     private void notifyWorkmatesSelections(int id, String text) {
-
-
         Notification notification = new NotificationCompat
                 .Builder(this.context, NotificationConfig.NOTIFICATION_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_logo)
                 .setContentTitle("Go4Lunch Time")
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setTimeoutAfter(6000)
                 .setAutoCancel(true)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(this.createNotificationContentIntent(id, text))
                 .build();
 
         this.notificationManager.notify(id, notification);
 
     }
 
+    private PendingIntent createNotificationContentIntent(int id, String text) {
+        // Create Intent and return PendingIntent
+        Intent notifyIntent = new Intent(this.context, ActivityResult.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        notifyIntent.putExtra(NotificationConfig.NOTIFICATION_ID, id);
+        notifyIntent.putExtra(NotificationConfig.NOTIFICATION_TEXT, text);
 
+        return PendingIntent.getActivity(
+                this.context, 0, notifyIntent, PendingIntent.FLAG_IMMUTABLE
+        );
+    }
+
+
+    private List<String> getNotificationTexts() {
+        List<String> notifications = new ArrayList<>();
+        if(!this.getSelections().isEmpty()) {
+            for(Selection selection: this.getSelections()) {
+                notifications.add(selection.getWorkmateName() + " is eating at " + selection.getRestaurantName());
+            }
+        }
+        return notifications;
+    }
+
+    private List<Selection> getSelections() {
+        // Mock
+        Selection selection1 = new Selection("resto1", "workmate1");
+        selection1.setWorkmateName("Janie");
+        selection1.setRestaurantName("Chez Jojo");
+        selection1.setId("selection1");
+
+        return Arrays.asList(selection1);
+    }
 
 }
